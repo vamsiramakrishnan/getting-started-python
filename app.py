66feefc9bb5b1da0159ac498a25ad476484b3416
Benno van den Berg <bennovandenberg@gmail.com>
2013-05-13 12:08:43 +0200

    Use OpenShift specific env var

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def cities():
     return resp
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 5000))
     app.run(host='0.0.0.0', port=port)
