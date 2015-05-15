dc2afe75f30f7616d9811945c959f91c53c9d91b
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2015-05-15 12:05:32 +0200

    no reload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return "Hello World"
 
 @app.route('/cities.json')
 def cities():
@@ -17,4 +17,4 @@ def cities():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
