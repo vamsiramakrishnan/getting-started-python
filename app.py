d9fb3a5311fe19072a1c9c217e8bc3bdda528550
Rik Gibson <rik.gibson@oracle.com>
2017-10-04 14:31:17 +0100

    Test now matches app code...

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route('/cities.json')
 def cities():
-    data = {"cities" : ["San Francisco", "Amsterdam", "Berlin", "New York", "Tokyo"]}
+    data = {"cities" : ["Amsterdam", "San Francisco", "Berlin", "New York", "Tokyo"]}
     resp = Response(json.dumps(data), status=200, mimetype='application/json')
     return resp
 
