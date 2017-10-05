3e358d2ad3e1890e1479b32b9ddc4ae32ca933a9
Rik Gibson <rik.gibson@oracle.com>
2017-10-05 16:54:56 +0100

    Fix test too...

--- app_test.py
+++ app_test.py
@@ -9,7 +9,7 @@ class CitiesTestCase(unittest.TestCase):
     tester = app.test_client(self)
     response = tester.get('/cities.json', content_type='application/json')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, json.dumps({"cities" : ["Amsterdam", "San Francisco", "Berlin", "New York", "Tokyo"]}))
+    self.assertEqual(response.data, json.dumps({"cities" : ["Amsterdam", "Berlin", "New York", "San Francisco", "Tokyo"]}))
 
 if __name__ == '__main__':
     unittest.main()
